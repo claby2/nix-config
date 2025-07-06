02ad08a25bb28d55ad6175a993d305c5dede5c59
Edward Wibowo <wibow9770@gmail.com>
2025-07-06 19:03:59 +0000

    feat: try agent forwarding

--- meta/default.nix
+++ meta/default.nix
@@ -1,6 +1,6 @@
 { ... }: {
   sshPublicKeys = {
     applin =
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDEONXTWVbwRMZJpZ+CJE2n1ELDBjXG39+Ogxdkf1BQcpufLngg0iteB46YcRkZcaErCa+EI2YjkiOrGMTrXfR050tSicG79i+1fjkzqnAe7lA4jDbOjnqn1v3iO8qBPpPr6Jtxg8LMMowGXIi69W36aBzIs73WEW2zN5bsbtCYr0zXBaLaGCfzr2kAzDuiFvPGrQMwlNDYS0EuNklt/RscFzwjRS5sJhi2IteNLbPRMzNYeDBhubgJd1l+tTxWgOo7X80cUjZ9dSytLMd+3ean8qTOC7h4AggUmKLEQ4e86TwyQG8uppeYfYNurI9QdnrMnilQfVC8b47mj3+ohBiYXiVZpvN0g1mgUgHzgg2xnWoqJS46DdKSeSBS0dE8FHOh/iAJPhd7DRk7nZL384uen0WdghUax+32yYj+sp/ofLcHM81sqjtzgUnnfEW+FYeD/KbWrdNz4zuXmFzM60XIlnF2yComBQnvsuEFrC95rYgBbu4qr0FqJFyGIIkEaMs=";
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMIgpfqrD63csQegPzBTBPcNJbzgdsBkJhDm/w1uchE+";
   };
 }
