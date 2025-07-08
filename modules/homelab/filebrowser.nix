d9b6a71ad07b7fe36ada75f74e8c149ead58f86c
Edward Wibowo <wibow9770@gmail.com>
2025-07-08 00:30:07 +0000

    feat: add filebrowser

---
+++ modules/homelab/filebrowser.nix
@@ -0,0 +1,25 @@
+{ config, lib, ... }:
+let cfg = config.homelab.filebrowser;
+in {
+  options.homelab.filebrowser = {
+    enable = lib.mkEnableOption "filebrowser";
+    port = lib.mkOption { type = lib.types.port; };
+    host = lib.mkOption { type = lib.types.str; };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.filebrowser = {
+      enable = true;
+      settings.port = cfg.port;
+    };
+
+    # Seems like the freshrss service in nixpkgs does not enable SSL...
+    # Forcing that here!
+    services.nginx.virtualHosts.${cfg.host} = {
+      addSSL = true;
+      enableACME = true;
+      locations."/" = { proxyPass = "http://127.0.0.1:${toString cfg.port}/"; };
+    };
+  };
+}
+
