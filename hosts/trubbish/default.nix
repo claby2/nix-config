af4f0f2fcbe2f3221874eba9431532c19094c03e
Edward Wibowo <wibow9770@gmail.com>
2025-07-28 22:50:09 -0400

    fix: use block dev uuids

--- hosts/trubbish/default.nix
+++ hosts/trubbish/default.nix
@@ -7,24 +7,25 @@
   # Boot configuration
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "ufshcd_pci" "xhci_pci" "uas" "sd_mod" "sdhci_pci" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   # File systems
   fileSystems."/" = {
-    device = "/dev/sda1";
+    device = "/dev/disk/by-uuid/16631731-a80f-4741-a968-94eb1f7bb15d";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/sda3";
+    device = "/dev/disk/by-uuid/58F0-A6A4";
     fsType = "vfat";
+    options = [ "fmask=0077" "dmask=0077" ];
   };
 
   swapDevices = [
-    { device = "/dev/sda2"; }
+    { device = "/dev/disk/by-uuid/a70859c3-3cea-4f8a-882e-16a5d0c468c8"; }
   ];
 
   # Networking
