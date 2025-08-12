6f3dfec3b473105317ba6a7f5a42d57948d78651
Edward Wibowo <wibow9770@gmail.com>
2025-08-12 14:16:19 +0000

    feat: configure wireguard server on altaria

--- hosts/altaria/default.nix
+++ hosts/altaria/default.nix
@@ -53,6 +53,49 @@
   networking.firewall = {
     enable = true;
     allowedTCPPorts = [ 80 443 ];
+    allowedUDPPorts = [ 51820 ]; # WireGuard
+  };
+  networking.nat = { # WireGuard
+    enable = true;
+    externalInterface = "eth0";
+    internalInterfaces = [ "wg0" ];
+  };
+
+  # WireGuard server configuration
+  age.secrets.wireguard-private-key.file = ./secrets/wireguard-private-key.age;
+  networking.wireguard.interfaces = {
+    wg0 = {
+      # Server IP and port
+      ips = [ "10.100.0.1/24" ];
+      listenPort = 51820;
+
+      # Placeholder for private key file
+      privateKeyFile = config.age.secrets.wireguard-private-key.path;
+
+      # Enable IP forwarding for the server
+      postSetup = ''
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+      '';
+
+      postShutdown = ''
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+      '';
+
+      peers = [
+        # Placeholder for peers - add client configurations here
+        # Example:
+        # {
+        #   publicKey = "client_public_key_here";
+        #   allowedIPs = [ "10.100.0.2/32" ];
+        # }
+      ];
+    };
+  };
+
+  # Enable IP forwarding
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+    "net.ipv6.conf.all.forwarding" = 1;
   };
 
   programs.zsh.loginShellInit = ''
