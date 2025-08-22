cf790ddb016a4f85f344fb34a390e6ec067ed90f
Edward Wibowo <wibow9770@gmail.com>
2025-08-23 05:46:05 +0700

    feat: make restic-onix accessible from applin

--- hosts/applin/default.nix
+++ hosts/applin/default.nix
@@ -1,4 +1,4 @@
-{ config, inputs, ... }: {
+{ config, inputs, pkgs, ... }: {
   hostclass.mac = {
     enable = true;
     motd = builtins.readFile "${inputs.self}/hosts/applin/applin";
@@ -8,11 +8,33 @@
   nix.settings.experimental-features = "nix-command flakes";
   nixpkgs.hostPlatform = "aarch64-darwin";
 
+  # === AGE
+  # NOTE: age.identityPaths looks at ssh host keys, but not personal keys. So,
+  # we must override age.identityPaths here.
+  age.identityPaths = [ "${config.users.users.claby2.home}/.ssh/id_ed25519" ];
+  age.secrets.onix-restic-repository.file =
+    ../onix/secrets/restic-repository.age;
+  age.secrets.onix-restic-password.file = ../onix/secrets/restic-password.age;
+  age.secrets.onix-restic-environment.file =
+    ../onix/secrets/restic-environment.age;
+
   # === SERVICES
   services.tailscale.enable = true;
 
   # === ENVIRONMENT
   environment.variables.TERM = "rxvt";
+  environment.systemPackages = [
+    # NOTE: We can take advantage of the fact that applin has access to onix's
+    # restic secrets and create a wrapper script.
+    (pkgs.writeScriptBin "restic-onix" ''
+      set -a
+      source ${config.age.secrets.onix-restic-environment.path}
+      RESTIC_PASSWORD_FILE=${config.age.secrets.onix-restic-password.path}
+      RESTIC_REPOSITORY_FILE=${config.age.secrets.onix-restic-repository.path}
+
+      exec ${pkgs.restic}/bin/restic "$@"
+    '')
+  ];
 
   # === USERS
   users.users.claby2 = {
