c0a9128c7cdacdceafdb2a5cb9bc1937a6a89cc8
Edward Wibowo <wibow9770@gmail.com>
2026-03-28 12:47:28 -0400

    feat(nvim): add peek.nvim plugin

--- apps/nvim/lua/plugins/init.lua
+++ apps/nvim/lua/plugins/init.lua
@@ -157,4 +157,14 @@ return {
 			{ "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
 		},
 	},
+	{
+		"toppair/peek.nvim",
+		event = { "VeryLazy" },
+		build = "deno task --quiet build:fast",
+		config = function()
+			require("peek").setup()
+			vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+			vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+		end,
+	},
 }
