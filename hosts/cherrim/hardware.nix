357fa167882b02d6d0333fbe6361317b72c4bb54
Edward Wibowo <wibow9770@gmail.com>
2026-07-28 23:42:35 +0000

    feat: change cherrim key and use mainline kernel

--- hosts/cherrim/hardware.nix
+++ hosts/cherrim/hardware.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }: {
+{ lib, pkgs, inputs, ... }: {
   # Raspberry Pi 4 support: pinned RPi kernel and WiFi/Bluetooth firmware.
   imports = [ inputs.nixos-hardware.nixosModules.raspberry-pi-4 ];
 
@@ -12,6 +12,7 @@
     fsType = "ext4";
   };
 
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages;
   # Raspberry Pi does not use GRUB
   boot.loader.grub.enable = false;
   boot.loader.generic-extlinux-compatible.enable = true;
